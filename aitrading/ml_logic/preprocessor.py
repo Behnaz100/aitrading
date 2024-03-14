deb4650f29ebb817cc5635d29c165774965ef574
Anthony Ahmed <awsalesforce2021@gmail.com>
2024-03-14 19:58:25 +0000

    anthony deployment

--- aitrading/ml_logic/preprocessor.py
+++ aitrading/ml_logic/preprocessor.py
@@ -7,7 +7,7 @@ from sklearn.pipeline import make_pipeline
 from sklearn.compose import ColumnTransformer, make_column_transformer
 from sklearn.preprocessing import OneHotEncoder, FunctionTransformer
 
-from taxifare.ml_logic.encoders import transform_time_features, transform_lonlat_features, compute_geohash
+from aitrading.ml_logic.encoders import transform_time_features, transform_lonlat_features, compute_geohash
 
 
 def preprocess_features(X: pd.DataFrame) -> np.ndarray:
